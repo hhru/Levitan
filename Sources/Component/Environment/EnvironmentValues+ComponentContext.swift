7baf4a6bd04625302a2c10469657274cfbde3dfc
Almaz Ibragimov <almazrafi@gmail.com>
2024-04-11 15:54:15 +0300

    Переменная окружения для получения всего контекста компонентов

---
+++ Sources/Component/Environment/EnvironmentValues+ComponentContext.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension EnvironmentValues {
+
+    /// Контекст компонентов целиком.
+    ///
+    /// Позволяет получить в SwiftUI-представлениях контекст компонентов,
+    /// например, чтобы определить их размер.
+    public var componentContext: ComponentContext {
+        ComponentContext(
+            environment: self,
+            overrides: [:]
+        )
+    }
+}
