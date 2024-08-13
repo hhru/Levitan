eb47bb4dcb32fe230bc079db71b05f75ed0905d9
Almaz Ibragimov <almazrafi@gmail.com>
2024-08-13 12:00:00 +0300

    Вертикальное выравнивание иконки в тексте

--- Sources/Text/Part/Extensions/ImageValue+TextPart.swift
+++ Sources/Text/Part/Extensions/ImageValue+TextPart.swift
@@ -16,7 +16,17 @@ extension ImageValue: TextPart {
             .foregroundColor(self.foregroundColor ?? foregroundColor)
             .uiImage
 
+        let imageHeight = uiImage.size.height
+        let capHeight = typography.font.uiFont.capHeight
+
         let attachment = NSTextAttachment(image: uiImage)
+
+        attachment.bounds = CGRect(
+            x: .zero,
+            y: (capHeight - imageHeight) * 0.5,
+            width: uiImage.size.width,
+            height: uiImage.size.height
+        )
 
         return NSAttributedString(attachment: attachment)
     }
