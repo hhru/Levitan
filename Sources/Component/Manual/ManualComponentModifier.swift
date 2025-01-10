6e3dac99cd3d3f99241b4e06e29786ba7a576cc0
Almaz Ibragimov <almazrafi@gmail.com>
2025-01-10 10:47:07 +0300

    Модификатор для ManualComponent

---
+++ Sources/Component/Manual/ManualComponentModifier.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+public protocol ManualComponentModifier: ComponentModifier {
+
+    func size<Content: ManualComponent>(
+        content: Content,
+        fitting size: CGSize,
+        context: ComponentContext
+    ) -> CGSize
+}
+
+extension ModifiedContent: ManualComponent where
+    Content: ManualComponent,
+    Modifier: ManualComponentModifier {
+
+    public func size(
+        fitting size: CGSize,
+        context: ComponentContext
+    ) -> CGSize {
+        modifier.size(
+            content: content,
+            fitting: size,
+            context: context
+        )
+    }
+}
