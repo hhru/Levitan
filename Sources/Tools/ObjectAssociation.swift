c75ccc5cfeecfe4381b23529653b6d043aa75ba0
a.maksimkin <a.maksimkin@hh.ru>
2025-10-20 11:40:42 +0300

    make ObjectAssociation safe

--- Sources/Tools/ObjectAssociation.swift
+++ Sources/Tools/ObjectAssociation.swift
@@ -3,6 +3,10 @@ import Foundation
 internal final class ObjectAssociation<Value>: @unchecked Sendable {
 
     private let policy: objc_AssociationPolicy
+    private let queue = DispatchQueue(
+        label: "object_association_queue",
+        attributes: .concurrent
+    )
 
     internal init(policy: objc_AssociationPolicy = .OBJC_ASSOCIATION_RETAIN_NONATOMIC) {
         self.policy = policy
@@ -10,19 +14,30 @@ internal final class ObjectAssociation<Value>: @unchecked Sendable {
 
     internal subscript(object: AnyObject) -> Value? {
         get {
-            objc_getAssociatedObject(
-                object,
-                Unmanaged.passRetained(self).toOpaque()
-            ) as? Value
+            queue.sync {
+                objc_getAssociatedObject(
+                    object,
+                    Unmanaged.passRetained(self).toOpaque()
+                ) as? Value
+            }
         }
 
         set {
-            objc_setAssociatedObject(
-                object,
-                Unmanaged.passRetained(self).toOpaque(),
-                newValue,
-                policy
-            )
+            nonisolated(unsafe) let unsafeObject = object
+            nonisolated(unsafe) let unsafeNewValue = newValue
+
+            queue.async(flags: .barrier) { [weak self] in
+                guard let self else {
+                    return
+                }
+
+                objc_setAssociatedObject(
+                    unsafeObject,
+                    Unmanaged.passRetained(self).toOpaque(),
+                    unsafeNewValue,
+                    policy
+                )
+            }
         }
     }
 }
