71e6b5864716959a549e092f917e96c7a14882da
Almaz Ibragimov <almazrafi@gmail.com>
2026-05-13 20:36:04 +0300

    Shape improvements (#61)

--- Sources/Token/Presentation/SwiftUI/Views/TokenValueView.swift
+++ Sources/Token/Presentation/SwiftUI/Views/TokenValueView.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 
-internal struct TokenValueView<Value: View>: View {
+internal struct TokenValueView<Value: View> {
 
     internal let token: Token<Value>
 
     @Environment(\.tokenTheme)
     internal var theme: TokenTheme
+}
+
+extension TokenValueView: View {
 
     internal var body: some View {
         token.resolve(for: theme)
