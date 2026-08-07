d16ab2baa71c4fecc4dffd6964aeb090e0187d3c
Almaz Ibragimov <almazrafi@gmail.com>
2026-08-07 14:00:54 +0300

    Public ComponentHostingController

--- Sources/Component/Hosting/ComponentHostingController.swift
+++ Sources/Component/Hosting/ComponentHostingController.swift
@@ -2,18 +2,18 @@
 import SwiftUI
 import UIKit
 
-internal final class ComponentHostingController<Content: View>: UIHostingController<Content> {
+public final class ComponentHostingController<Content: View>: UIHostingController<Content> {
 
     private var intrinsicContentSize: CGSize? {
         didSet { intrinsicContentSizeInvalidation?() }
     }
 
-    internal var intrinsicContentSizeInvalidation: (@MainActor () -> Void)?
+    public var intrinsicContentSizeInvalidation: (@MainActor () -> Void)?
 
     // До iOS 16 UIHostingController самовольно отображает скрытую панель навигации.
     // Чтобы это предотвратить, переопределяем для него navigationController
     // и возвращаем nil для iOS старше 16.
-    internal override var navigationController: UINavigationController? {
+    public override var navigationController: UINavigationController? {
         if #available(iOS 16.0, tvOS 16.0, *) {
             return super.navigationController
         }
@@ -21,7 +21,7 @@ internal final class ComponentHostingController<Content: View>: UIHostingControl
         return nil
     }
 
-    internal init(
+    public init(
         rootView: Content,
         intrinsicContentSizeInvalidation: (@MainActor () -> Void)? = nil
     ) {
@@ -31,11 +31,11 @@ internal final class ComponentHostingController<Content: View>: UIHostingControl
     }
 
     @available(*, unavailable)
-    internal required dynamic init?(coder aDecoder: NSCoder) {
+    public required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    internal override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         if #available(iOS 16.4, tvOS 16.4, *) {
@@ -47,7 +47,7 @@ internal final class ComponentHostingController<Content: View>: UIHostingControl
         view.backgroundColor = .clear
     }
 
-    internal override func viewDidLayoutSubviews() {
+    public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
         if intrinsicContentSize != view.intrinsicContentSize {
